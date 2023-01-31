1718ed3fa87161cd19e4e19d23b64d9acfaccc5f
li-ruida <2605209238@qq.com>
2023-01-31 10:25:26 +0800

    增加部分测试数据
    2023/1/31

--- document/MySQL/data/post_data.sql
+++ document/MySQL/data/post_data.sql
@@ -8,3 +8,14 @@ insert into `post_table` (`id`, `name`, `writter`, `visible`, `content`, `theme`
 insert into `post_table` (`id`, `name`, `writter`, `visible`, `content`, `theme`, `time`) values (8, '火红的萨日朗', 2, 1, '东北农业大学', 1, '2022-11-25 06:44:22');
 insert into `post_table` (`id`, `name`, `writter`, `visible`, `content`, `theme`, `time`) values (9, '本草纲目', 2, 1, '东南农业大学', 1, '2022-08-10 04:11:16');
 insert into `post_table` (`id`, `name`, `writter`, `visible`, `content`, `theme`, `time`) values (10, '像我这样的人', 2, 1, '东南理工大学', 1, '2022-11-03 10:57:54');
+
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (21, '2022-10-14 03:42:16', 1, 1, '周五', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (22, '2022-07-30 08:25:24', 2, 1, '周二', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (23, '2022-12-07 15:51:10', 3, 1, '周三', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (24, '2022-09-22 07:56:35', 4, 1, '周五', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (25, '2022-01-08 14:47:53', 5, 1, '周二', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (26, '2022-02-06 19:55:19', 6, 1, '周三', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (27, '2022-03-10 20:24:53', 7, 1, '周日', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (28, '2022-11-30 03:31:51', 8, 1, '周日', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (29, '2022-09-05 23:12:27', 9, 1, '周二', 2, 0);
+insert into `comment_table` (`id`, `time`, `writter`, `visible`, `content`, `post`, `reply`) values (30, '2022-08-02 09:30:25', 10, 1, '周二', 2, 0);
